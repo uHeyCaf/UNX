04b2792aebc1c02768f5ac4e78b44f41a6091db6
uHeyCaf <158849925+uHeyCaf@users.noreply.github.com>
2025-06-22 16:11:50 -0400

    Update API.lua

--- API.lua
+++ API.lua
@@ -3,9 +3,9 @@
 local plr = game.Players.LocalPlayer
 
 local BannedUsers = {
-    {UserId = 1, Reason = "Hi Roblox Big Fan"}
-    {UserId = 1845277449, Reason = "No Reason Given"}
-    {UserId = 1932649501, Reason = "No Reason Given"}
+    {UserId = 1, Reason = "Hi Roblox Big Fan"},
+    {UserId = 1845277449, Reason = ""},
+    {UserId = 1932649501, Reason = ""}
 }
 
 local BannedGames = {
@@ -14,7 +14,9 @@ local BannedGames = {
 
 for _, banInfo in ipairs(BannedUsers) do
     if plr.UserId == banInfo.UserId then
-        plr.PlayerGui.UNXHubUI:Destroy()
+        if plr:FindFirstChild("PlayerGui") and plr.PlayerGui:FindFirstChild("UNXHubUI") then
+            plr.PlayerGui.UNXHubUI:Destroy()
+        end
         print("[API]: User is banned from using UNXHub. Reason: " .. banInfo.Reason)
         plr:Kick("Sorry, but you are banned from using the main loadstring.\nReason: " .. banInfo.Reason)
         return
@@ -23,7 +25,9 @@ end
 
 for _, banInfo in ipairs(BannedGames) do
     if game.PlaceId == banInfo.PlaceId then
-        plr.PlayerGui.UNXHubUI:Destroy()
+        if plr:FindFirstChild("PlayerGui") and plr.PlayerGui:FindFirstChild("UNXHubUI") then
+            plr.PlayerGui.UNXHubUI:Destroy()
+        end
         print("[API]: Game is banned from using UNXHub. Reason: " .. banInfo.Reason)
         plr:Kick("Sorry, but the game you are playing is banned from using UNXHub.\nReason: " .. banInfo.Reason)
         return
