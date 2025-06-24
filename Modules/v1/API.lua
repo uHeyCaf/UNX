4031fb64d04d319e6ac4397abda1ee222c90277e
uHeyCaf <158849925+uHeyCaf@users.noreply.github.com>
2025-06-24 03:44:43 -0400

    Create API.lua

---
+++ Modules/v1/API.lua
@@ -0,0 +1,33 @@
+-- UNXHub "API" - Please Get Out.
+
+local plr = game.Players.LocalPlayer
+
+local BannedUsers = {
+    {UserId = 1, Reason = "Hi Roblox Big Fan"},
+    {UserId = 1845277449, Reason = ""},
+    {UserId = 1932649501, Reason = ""}
+}
+
+local BannedGames = {
+    {PlaceId = 4924922222, Reason = "Please get your stinky Brookhaven out of here."}
+}
+
+for _, banInfo in ipairs(BannedUsers) do
+    if plr.UserId == banInfo.UserId then
+        game.CoreGui.UNXHubUI:Destroy()
+        print("[API]: User is banned from using UNXHub. Reason: " .. banInfo.Reason)
+        plr:Kick("Sorry, but you are banned from using the main loadstring.\nReason: " .. banInfo.Reason)
+        return
+    end
+end
+
+for _, banInfo in ipairs(BannedGames) do
+    if game.PlaceId == banInfo.PlaceId then
+        game.CoreGui.UNXHubUI:Destroy()
+        print("[API]: Game is banned from using UNXHub. Reason: " .. banInfo.Reason)
+        plr:Kick("Sorry, but the game you are playing is banned from using UNXHub.\nReason: " .. banInfo.Reason)
+        return
+    end
+end
+
+print("[API]: All checks done. User is not banned and the game is not restricted.")
